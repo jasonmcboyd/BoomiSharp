8e491da7120a2d3383e919613999d8cbe302fb1a
Jason Boyd <jason.boyd.ce@gmail.com>
2018-09-13 16:01:21 -0700

    Added NuGet package publishing

---
+++ build/Publish-BoomiSharp.ps1
@@ -0,0 +1,3 @@
+﻿dotnet pack ..\src\BoomiSharp\BoomiSharp.csproj --configuration Release --output "$PSScriptRoot\..\publish"
+dotnet pack ..\src\BoomiSharp.Dtos\BoomiSharp.Dtos.csproj --configuration Release --output "$PSScriptRoot\..\publish"
+dotnet pack ..\src\BoomiSharp.Query\BoomiSharp.Query.csproj --configuration Release --output "$PSScriptRoot\..\publish"
