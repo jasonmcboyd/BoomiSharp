d0ec60d208943acb3b877bc4e1093f1683b50ae5
Jason Boyd <jason.boyd.ce@gmail.com>
2018-09-14 07:56:04 -0700

    Updated publish script and incremented versions

--- build/Publish-BoomiSharp.ps1
+++ build/Publish-BoomiSharp.ps1
@@ -1,3 +1,3 @@
-﻿dotnet pack ..\src\BoomiSharp\BoomiSharp.csproj --configuration Release --output "$PSScriptRoot\..\publish"
-dotnet pack ..\src\BoomiSharp.Dtos\BoomiSharp.Dtos.csproj --configuration Release --output "$PSScriptRoot\..\publish"
-dotnet pack ..\src\BoomiSharp.Query\BoomiSharp.Query.csproj --configuration Release --output "$PSScriptRoot\..\publish"
+﻿dotnet pack "$PSScriptRoot\..\src\BoomiSharp\BoomiSharp.csproj" --configuration Release --output "$PSScriptRoot\..\publish"
+dotnet pack "$PSScriptRoot\..\src\BoomiSharp.Dtos\BoomiSharp.Dtos.csproj" --configuration Release --output "$PSScriptRoot\..\publish"
+dotnet pack "$PSScriptRoot\..\src\BoomiSharp.Query\BoomiSharp.Query.csproj" --configuration Release --output "$PSScriptRoot\..\publish"
