4864b438e63c5b91da9b031a727bac88edde9536
Jason Boyd <jason.boyd.ce@gmail.com>
2018-09-14 21:59:16 -0700

    Added helpful constructors to the ProcessExecution class

--- src/BoomiSharp.Dtos/BoomiObjects/ProcessExecution.cs
+++ src/BoomiSharp.Dtos/BoomiObjects/ProcessExecution.cs
@@ -1,9 +1,31 @@
 ﻿using System;
+using System.Linq;
 
 namespace BoomiSharp.Dtos.BoomiObjects
 {
     public class ProcessExecution
     {
+        public ProcessExecution()
+        {
+        }
+
+        public ProcessExecution(Guid processId, Guid atomId, params (string Name, string Value)[] processProperties)
+        {
+            this.ProcessId = processId;
+            this.AtomId = AtomId;
+            if (ProcessProperties != null)
+            {
+                this.ProcessProperties =
+                    processProperties
+                    .Select(x => new ProcessExecution.ProcessProperty
+                    {
+                        Name = x.Name,
+                        Value = x.Value
+                    })
+                    .ToArray();
+            }
+        }
+
         public Guid ProcessId { get; set; }
         public Guid AtomId { get; set; }
         public ProcessProperty[] ProcessProperties { get; set; }
